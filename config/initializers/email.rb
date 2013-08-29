770f91ce1ad6dd7a7a388a3cc4f851a975d804f0
Franco Catena <francocatena@gmail.com>
2013-08-29 17:34:28 -0300

    Initial project Sales commit =)

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,4 +1,4 @@
-X0Y0::Application.configure do
+Sales::Application.configure do
   config.action_mailer.default_url_options = {
     host: APP_CONFIG['host'],
     protocol: (Rails.env.production? ? 'https' : 'http')
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-X0Y0::Application.config.session_store :cookie_store, key: '_x0y0_session'
+Sales::Application.config.session_store :cookie_store, key: '_sales_session'
