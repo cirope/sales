dc1f5d26b91e171e46c073ccb61d4ce448888174
Franco Catena <francocatena@gmail.com>
2013-09-05 22:40:02 -0300

    Product reservation with icon now

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def link_to_reservation product
+    link_to new_product_reservation_path(product), title: t('.new_reservation') do
+      content_tag :span, nil, class: 'glyphicon glyphicon-shopping-cart'
+    end
+  end
 end
